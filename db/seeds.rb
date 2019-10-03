ad3add676c591fed7ec231219582c6b5d7a5714f
bojanKalanj <bojan.kalanj@gmail.com>
2019-10-03 10:52:03 +0200

    Seed dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Item.create(title:"Psilocybin", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerci", price: 30)
+  Item.create(title:"Mushrooms", desc:"on ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum", price: 150)
+  Item.create(title:"Spore", desc:" aute irure dolor in reprehenderit in voluptate velit esse cillum", price: 150)
+  Item.create(title:"Edible", desc:"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum", price: 150)
+  Item.create(title:"Poisonous", desc:"ip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum", price: 150)
+end
